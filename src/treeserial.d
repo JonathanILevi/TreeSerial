0a0f5316a88baae9adb77c05ae2aef527f6d61d5
Jonathan <JonathanILevi@gmail.com>
2020-05-12 16:23:39 -0500

    Floats and Doubles can use basic number serialization function (and should handle endianness)

--- src/treeserial.d
+++ src/treeserial.d
@@ -6,22 +6,15 @@ import std.bitmanip;
 enum NoLength;
 
 template serialize(Ts...) {
-	ubyte[] serialize(T)(T data) if(isIntegral!T || isSomeChar!T || isBoolean!T) {
+	ubyte[] serialize(T)(T data) if(isIntegral!T || isSomeChar!T || isBoolean!T || isFloatOrDouble!T) {
 		return nativeToLittleEndian(data).dup;
 	}
 	////ubyte[T.sizeof] serialize(T)(T data) if(isIntegral!T || isSomeChar!T || isBoolean!T) {
 	////	return nativeToLittleEndian(data).dup;
 	////}
-	
-	ubyte[] serialize(T)(T data) if(isFloatingPoint!T) {
-		return (cast(ubyte*) cast(void*) (&data))[0..T.sizeof].dup;
-	}
-	////ubyte[T.sizeof] serialize(T)(T data) if(isFloatingPoint!T) {
-	////	return (cast(ubyte*) cast(void*) (&data))[0..T.sizeof].dup;
-	////}
 }
 template deserialize(T, Ts...) {
-	static if (isIntegral!T || isSomeChar!T || isBoolean!T) {
+	static if (isIntegral!T || isSomeChar!T || isBoolean!T || isFloatOrDouble!T) {
 		////T deserialize(scope ubyte[T.sizeof] data) {
 		////	return littleEndianToNative!T(data);
 		////}
@@ -31,18 +24,13 @@ template deserialize(T, Ts...) {
 			return littleEndianToNative!T(data[0..T.sizeof]);
 		}
 	}
-	static if (isFloatingPoint!T) {
-		////T deserialize(scope ubyte[T.sizeof] data) {
-		////	return *(cast(T*) cast(void*) data.ptr);
-		////}
-		T deserialize(ref ubyte[] data) {
-			scope (success)
-				data = data[T.sizeof..$];
-			return *(cast(T*) cast(void*) data.ptr);
-		}
-	}
 }
 
-
+/// Copied from phobas/std/bitmanip
+private template isFloatOrDouble(T)
+{
+    enum isFloatOrDouble = isFloatingPoint!T &&
+                           !is(Unqual!(FloatingPointTypeOf!T) == real);
+}
 
  
